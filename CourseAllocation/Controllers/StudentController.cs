55d4661fb3aa2eb1b4b35119b753d8c8c23db40d
mgildea <mattgildea@hotmail.com>
2015-11-19 10:54:02 -0800

    Student View

---
+++ CourseAllocation/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseAllocation.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+
+           var id = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(User.Identity.GetUserId());
+
+          //  string id = User.Identity.GetUserId();
+
+            return View(id);
+        }
+    }
+}
